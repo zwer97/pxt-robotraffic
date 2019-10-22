8e76513c005a465053df51e93cc14643350f455e
ZwerInc <sergey.yatsuk97@gmail.com>
2019-10-22 13:57:35 +0300

    kek

---
+++ built/binary.asm
@@ -0,0 +1,820 @@
+; Interface tables: 0/0 (NaN%)
+; Virtual methods: 0 / 0
+; generated code sizes (bytes): 1472 (incl. 770 user, 652 helpers, 14 vtables, 36 lits); src size 0
+; assembly: 990 lines; density: Infinity bytes/stmt; (0 stmts)
+; total bytes: 216512 (89.2% of 237.0k flash with 26176 free)
+; peep hole pass: 22 instructions removed and 22 updated
+; peep hole pass: 0 instructions removed and 0 updated
+
+
+; start
+    .startaddr 0x34800
+    .hex 708E3B92C615A841C49866C975EE5197 ; magic number
+    .hex 4DB2EA85EC434CF8 ; hex template hash
+    .hex E2F723A44942FF68 ; program hash
+    .short 1   ; num. globals
+    .short 0 ; patched with number of 64 bit words resulting from assembly
+    .word _pxt_config_data
+    .short 0 ; patched with comm section size
+    .short 1 ; number of globals that are not pointers (they come first)
+    .word _pxt_iface_member_names
+    .word _pxt_lambda_trampoline@fn
+    .word _pxt_perf_counters
+    .word _pxt_restore_exception_state@fn
+    .word _str0 ; name
+    .section code
+_pxt_lambda_trampoline:
+    push { r4, r5, r6, r7, lr}
+    mov r4, r8
+    mov r5, r9
+    mov r6, r10
+    mov r7, r11
+    push {r4, r5, r6, r7} ; save high registers
+    mov r4, r1
+    mov r5, r2
+    mov r6, r3
+    mov r7, r0
+    bl _inst_builtin4_validate_0
+    mov r0, sp
+    push {r4, r5, r6, r7} ; push args and the lambda
+    mov r1, sp
+    bl pxt::pushThreadContext
+    mov r6, r0          ; save ctx or globals
+    mov r5, r7          ; save lambda for closure
+    ldr r0, [r5, #8]    ; ld fnptr
+    movs r4, #3         ; 3 args
+    blx r0              ; execute the actual lambda
+    mov r7, r0          ; save result
+    @dummystack 4
+    add sp, #4*4        ; remove arguments and lambda
+    mov r0, r6   ; or pop the thread context
+    bl pxt::popThreadContext
+    mov r0, r7 ; restore result
+    pop {r4, r5, r6, r7} ; restore high registers
+    mov r8, r4
+    mov r9, r5
+    mov r10, r6
+    mov r11, r7
+    pop { r4, r5, r6, r7, pc}
+    .section code
+; r0 - try frame
+; r1 - handler PC
+_pxt_save_exception_state:
+    push {r0, lr}
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::beginTry
+    pop {r1, r4}
+    str r1, [r0, #1*4] ; PC
+    mov r1, sp
+    str r1, [r0, #2*4] ; SP
+    str r5, [r0, #3*4] ; lambda ptr
+    bx r4
+    .section code
+; r0 - try frame
+; r1 - thread context
+_pxt_restore_exception_state:
+    mov r6, r1
+    ldr r1, [r0, #2*4] ; SP
+    mov sp, r1
+    ldr r5, [r0, #3*4] ; lambda ptr
+    ldr r1, [r0, #1*4] ; PC
+    movs r0, #1
+    orrs r1, r0
+    bx r1
+    .section code
+_pxt_stringConv:
+    lsls r2, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r2, [r3, #8]
+    cmp r2, #1
+    bne .notstring
+    bx lr
+.notstring:
+    ldr r7, [r3, #4*8]
+    cmp r7, #0
+    beq .fail
+    push {r0, lr}
+    movs r4, #1
+    blx r7
+    str r0, [sp, #0]
+    b .numops
+.fail:
+    push {r0, lr}
+.numops:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl numops::toString
+    pop {r1, pc}      
+    .section code
+_pxt_buffer_get:
+    lsls r4, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r4, [r3, #8]
+    cmp r4, #3
+    bne .fail
+    asrs r1, r1, #1
+    bcc .notint
+    ldr r4, [r0, #4]
+    cmp r1, r4
+    bhs .oob
+    adds r4, r0, r1
+    ldrb r0, [r4, #8]
+    lsls r0, r0, #1
+    adds r0, #1
+    bx lr
+.notint:
+    lsls r1, r1, #1
+    push {lr, r0, r2}      
+    mov r0, r1
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::toInt
+    mov r1, r0
+    pop {r0, r2}
+.doop:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl Array_::getAt
+    lsls r0, r0, #1
+    adds r0, #1
+    pop {pc}
+.fail:
+    bl pxt::failedCast
+.oob:
+    movs r0, #1 ; 0 or undefined
+    bx lr
+    .section code
+_pxt_array_get:
+    lsls r4, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r4, [r3, #8]
+    cmp r4, #6
+    bne .fail
+    asrs r1, r1, #1
+    bcc .notint
+    ldrh r4, [r0, #8]
+    cmp r1, r4
+    bhs .oob
+    lsls r1, r1, #2
+    ldr r4, [r0, #4]
+    ldr r0, [r4, r1]
+    bx lr
+.notint:
+    lsls r1, r1, #1
+    push {lr, r0, r2}      
+    mov r0, r1
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::toInt
+    mov r1, r0
+    pop {r0, r2}
+.doop:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl Array_::getAt
+    pop {pc}
+.fail:
+    bl pxt::failedCast
+.oob:
+    movs r0, #0 ; 0 or undefined
+    bx lr
+    .section code
+_pxt_buffer_set:
+    lsls r4, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r4, [r3, #8]
+    cmp r4, #3
+    bne .fail
+    asrs r1, r1, #1
+    bcc .notint
+    ldr r4, [r0, #4]
+    cmp r1, r4
+    bhs .oob
+    adds r4, r0, r1
+    strb r2, [r4, #8]
+    bx lr
+.notint:
+    lsls r1, r1, #1
+    push {lr, r0, r2}      
+    mov r0, r1
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::toInt
+    mov r1, r0
+    pop {r0, r2}
+.doop:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl Array_::setAt
+    pop {pc}
+.fail:
+    bl pxt::failedCast
+.oob:
+    push {lr}
+    b .doop
+    .section code
+_pxt_array_set:
+    lsls r4, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r4, [r3, #8]
+    cmp r4, #6
+    bne .fail
+    asrs r1, r1, #1
+    bcc .notint
+    ldrh r4, [r0, #8]
+    cmp r1, r4
+    bhs .oob
+    lsls r1, r1, #2
+    ldr r4, [r0, #4]
+    str r2, [r4, r1]
+    bx lr
+.notint:
+    lsls r1, r1, #1
+    push {lr, r0, r2}      
+    mov r0, r1
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::toInt
+    mov r1, r0
+    pop {r0, r2}
+.doop:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl Array_::setAt
+    pop {pc}
+.fail:
+    bl pxt::failedCast
+.oob:
+    push {lr}
+    b .doop
+    .section code
+_pxt_map_get:
+    lsls r4, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r4, [r3, #8]
+    cmp r4, #8
+    bne .notmap
+    push {lr}
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxtrt::mapGetByString
+    pop {pc}
+.notmap:
+    mov r4, r3 ; save VT
+    push {r0, lr}
+    mov r0, r1
+    bl pxtrt::lookupMapKey
+    mov r1, r0 ; put key index in r1
+    ldr r0, [sp, #0] ; restore obj pointer
+    mov r3, r4 ; restore vt
+    bl .dowork
+    add sp, #4*1 ; pop locals 1
+    pop {pc}
+.dowork:
+    ldr r2, [r3, #12] ; load mult
+    movs r7, r2
+    beq .objlit ; built-in types have mult=0
+    muls r7, r1
+    lsrs r7, r2
+    lsls r7, r7, #1 ; r7 - hash offset
+    ldr r3, [r3, #4] ; iface table
+    adds r3, r3, r7
+; r0-this, r1-method idx, r2-free, r3-hash entry, r4-num args, r7-free
+    ldrh r2, [r3, #0] ; r2-offset of descriptor
+    ldrh r7, [r2, r3] ; r7-method idx
+    cmp r7, r1
+    beq .hit
+    adds r3, #2
+    ldrh r2, [r3, #0] ; r2-offset of descriptor
+    ldrh r7, [r2, r3] ; r7-method idx
+    cmp r7, r1
+    beq .hit
+    adds r3, #2
+    ldrh r2, [r3, #0] ; r2-offset of descriptor
+    ldrh r7, [r2, r3] ; r7-method idx
+    cmp r7, r1
+    beq .hit
+    movs r0, #0 ; undefined
+    bx lr
+.hit:
+    adds r3, r3, r2 ; r3-descriptor
+    ldr r2, [r3, #4]
+    lsls r7, r2, #31
+    beq .field
+    movs r4, #1
+    bx r2
+.field:
+    ldr r0, [r0, r2] ; load field
+    bx lr
+.objlit:
+.fail:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::failedCast
+.fail2:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::missingProperty
+    .section code
+_pxt_map_set:
+    lsls r4, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r4, [r3, #8]
+    cmp r4, #8
+    bne .notmap
+    push {lr}
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxtrt::mapSetByString
+    pop {pc}
+.notmap:
+    mov r4, r3 ; save VT
+    push {r0, r2, lr}
+    mov r0, r1
+    bl pxtrt::lookupMapKey
+    mov r1, r0 ; put key index in r1
+    ldr r0, [sp, #0] ; restore obj pointer
+    mov r3, r4 ; restore vt
+    bl .dowork
+    add sp, #4*2 ; pop locals 2
+    pop {pc}
+.dowork:
+    ldr r2, [r3, #12] ; load mult
+    movs r7, r2
+    beq .objlit ; built-in types have mult=0
+    muls r7, r1
+    lsrs r7, r2
+    lsls r7, r7, #1 ; r7 - hash offset
+    ldr r3, [r3, #4] ; iface table
+    adds r3, r3, r7
+; r0-this, r1-method idx, r2-free, r3-hash entry, r4-num args, r7-free
+    ldrh r2, [r3, #0] ; r2-offset of descriptor
+    ldrh r7, [r2, r3] ; r7-method idx
+    cmp r7, r1
+    beq .hit
+    adds r3, #2
+    ldrh r2, [r3, #0] ; r2-offset of descriptor
+    ldrh r7, [r2, r3] ; r7-method idx
+    cmp r7, r1
+    beq .hit
+    adds r3, #2
+    ldrh r2, [r3, #0] ; r2-offset of descriptor
+    ldrh r7, [r2, r3] ; r7-method idx
+    cmp r7, r1
+    bne .fail2      
+.hit:
+    adds r3, r3, r2 ; r3-descriptor
+    ldr r2, [r3, #4]
+    lsls r7, r2, #31
+    beq .field
+; check for next descriptor
+    ldrh r7, [r3, #8]
+    cmp r7, r1
+    bne .fail2 ; no setter!
+    ldr r2, [r3, #12]
+    movs r4, #2
+    bx r2
+.field:
+    ldr r3, [sp, #4] ; ld-val
+    str r3, [r0, r2] ; store field
+    bx lr
+.objlit:
+.fail:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::failedCast
+.fail2:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::missingProperty
+_code_end:
+    .section code
+_inst_builtin4_validate_0:
+    lsls r2, r0, #30
+    bne .fail
+    cmp r0, #0
+    beq .fail
+    ldr r3, [r0, #0]
+; vtable in R3
+    ldrh r2, [r3, #8]
+    cmp r2, #4
+    bne .fail
+    bx lr
+.fail:
+    mov r7, sp
+    str r7, [r6, #4]
+    bl pxt::failedCast
+_numops_adds:
+    @scope _numops_adds
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r2, r1, #1
+    adds r2, r0, r2
+    bvs .boxed
+    movs r0, r2
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::adds
+                    add sp, #8
+                    pop {pc}
+_numops_subs:
+    @scope _numops_subs
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r2, r1, #1
+    subs r2, r0, r2
+    bvs .boxed
+    movs r0, r2
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::subs
+                    add sp, #8
+                    pop {pc}
+_numops_ands:
+    @scope _numops_ands
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    ands r0, r1
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::ands
+                    add sp, #8
+                    pop {pc}
+_numops_orrs:
+    @scope _numops_orrs
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    orrs r0, r1
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::orrs
+                    add sp, #8
+                    pop {pc}
+_numops_eors:
+    @scope _numops_eors
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    eors r0, r1
+    adds r0, r0, #1
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::eors
+                    add sp, #8
+                    pop {pc}
+_numops_lsls:
+    @scope _numops_lsls
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    ; r3 := (r1 >> 1) & 0x1f
+    lsls r3, r1, #26
+    lsrs r3, r3, #27
+    asrs r2, r0, #1
+    lsls r2, r3
+    lsrs r3, r2, #30
+    beq .ok
+    cmp r3, #3
+    bne .boxed
+.ok:
+    lsls r0, r2, #1
+    adds r0, r0, #1
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::lsls
+                    add sp, #8
+                    pop {pc}
+_numops_lsrs:
+    @scope _numops_lsrs
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    ; r3 := (r1 >> 1) & 0x1f
+    lsls r3, r1, #26
+    lsrs r3, r3, #27
+    asrs r2, r0, #1
+    lsrs r2, r3
+    lsrs r3, r2, #30
+    bne .boxed
+    lsls r0, r2, #1
+    adds r0, r0, #1
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::lsrs
+                    add sp, #8
+                    pop {pc}
+_numops_asrs:
+    @scope _numops_asrs
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    ; r3 := (r1 >> 1) & 0x1f
+    lsls r3, r1, #26
+    lsrs r3, r3, #27
+    asrs r0, r3
+    movs r2, #1
+    orrs r0, r2
+    blx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                    bl numops::asrs
+                    add sp, #8
+                    pop {pc}
+@scope _numops_toInt
+_numops_toInt:
+    asrs r0, r0, #1
+    bcc .over
+    blx lr
+.over:
+    lsls r0, r0, #1
+    push {lr}
+mov r7, sp
+    str r7, [r6, #4]
+bl pxt::toInt
+pop {pc}
+_numops_fromInt:
+    lsls r2, r0, #1
+    asrs r1, r2, #1
+    cmp r0, r1
+    bne .over2
+    adds r0, r2, #1
+    blx lr
+.over2:
+    push {lr}
+mov r7, sp
+    str r7, [r6, #4]
+bl pxt::fromInt
+pop {pc}
+.section code
+_cmp_lt:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    blt .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::lt
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+.section code
+_cmp_gt:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    bgt .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::gt
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+.section code
+_cmp_le:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    ble .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::le
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+.section code
+_cmp_ge:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    bge .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::ge
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+.section code
+_cmp_eq:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    beq .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::eq
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+.section code
+_cmp_eqq:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    beq .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::eqq
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+.section code
+_cmp_neq:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    bne .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::neq
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+.section code
+_cmp_neqq:
+    lsls r2, r0, #31
+    beq .boxed
+    lsls r2, r1, #31
+    beq .boxed
+    subs r0, r1
+    bne .true
+.false:
+    movs r0, #0
+    bx lr
+.true:
+    movs r0, #1
+    bx lr
+.boxed:
+    push {lr, r0, r1}      
+                    mov r7, sp
+    str r7, [r6, #4]
+                        bl numops::neqq
+                        bl numops::toBoolDecr
+                        cmp r0, #0
+                    add sp, #8
+                    pop {pc}
+_helpers_end:
+.balign 4
+_pxt_iface_member_names:
+    .word 1
+    .word _str1  ; 0 .
+    .word 0
+_vtables_end:
+.balign 4
+_pxt_config_data:
+    .word 0
+.balign 4
+_str1:
+ .word pxt::string_inline_ascii_vt
+        .short 0
+        .string ""
+.balign 4
+_str0:
+ .word pxt::string_inline_ascii_vt
+        .short 11
+        .string "robotraffic"
+.balign 4
+.section code
+_pxt_perf_counters:
+    .word 0
+_literals_end:
